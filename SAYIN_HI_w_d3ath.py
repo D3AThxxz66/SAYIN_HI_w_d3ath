44feab2b471f87f03afa72d8519c93c3be204ada
D£ATh!xxx666 <dfcvdeath@gmail.com>
2025-05-21 02:29:57 -0600

    Hi, 1 here!
    Bla, bla, bla!

---
+++ SAYIN_HI_w_d3ath.py
@@ -0,0 +1,22 @@
+name = "D£ATh!xxx666"
+mission = "To create pieces of Software that are pure art made with love and passion."
+slogan = "Get f'kin blazted, and then you go program with some corny a$$ music. That works!"
+
+print (
+    '''Hello, I like to be called, ''', (name), "."
+    '''
+And this is my final, and finally successful attempt at becoming a great programmer and artist in this field.
+My mission is: ''', (mission), 
+    '''
+If I had an slogan, it would be...
+    ''', (slogan), sep = '"'
+    )
+
+print (
+    "\n... And u know what els'? I love my two dogs, Mia and Dolsa. And my girlfriend Ariana. And I'm f'kin high rn, bye."
+)
+
+''' MADAFACKIN' COMMENT R' HERE!
+What it does, is that it shows in the console a visual que of what you're doing.
+This helps to confirm if everything is working and will display right in the screen.
+'''
